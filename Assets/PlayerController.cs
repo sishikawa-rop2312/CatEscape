fc33b45b85eacc8c74e4f5175b490d241714d945
sishikawa-rop2312 <155729870+sishikawa-rop2312@users.noreply.github.com>
2024-05-08 17:11:15 +0900

    PlayerController作成

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 左矢印押下
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0);
+        }
+
+        // 右矢印押下 
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(3, 0, 0);
+        }
+    }
+}
